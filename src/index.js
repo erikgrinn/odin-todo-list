5d78cd8d11f0ecf5e39aae8323ac6f820248504f
Erik Grinn <grinn.erik@gmail.com>
2024-09-02 21:08:14 -0400

    replace spaces with hyphens project title class names

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import "./styles.css";
 import {storageAvailable} from "./storage.js";
 
-if (storageAvailable("localStorage")) {
-    console.log('Yippee! We can use localStorage awesomeness')
-  } else {
-    console.log('Too bad, no localStorage for us')
-  }
+// if (storageAvailable("localStorage")) {
+//     console.log('Yippee! We can use localStorage awesomeness')
+//   } else {
+//     console.log('Too bad, no localStorage for us')
+//   }
 
 
 const projectTitles = document.getElementById('projectTitles')
 const projectTemplate = document.createElement('button')
 const addProjectBtn = document.getElementById('addProject')
-addProjectBtn.addEventListener('click', () => {
+addProjectBtn.addEventListener('click', addProjectTitle)
+
+function addProjectTitle(event) {
+    // const dialog = document.createElement('dialog')
     const projName = prompt('enter project title')
     let newProj = projectTemplate.cloneNode()
     newProj.textContent = projName
-    newProj.setAttribute('class', newProj.textContent)
+    newProj.setAttribute('class', newProj.textContent.replace(/\s+/g, '-')) // class names can't have spaces
     projectTitles.appendChild(newProj)
-})
+}
+
+
 console.log(projectTitles)
