d3d6b149ff7a02a6c9d0e1971f4d01cee2e48766
Erik Grinn <grinn.erik@gmail.com>
2024-09-02 23:28:37 -0400

    renaming main content classes/ids

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import "./styles.css";
-import {storageAvailable} from "./storage.js";
-
-// if (storageAvailable("localStorage")) {
-//     console.log('Yippee! We can use localStorage awesomeness')
-//   } else {
-//     console.log('Too bad, no localStorage for us')
-//   }
+// import {storageAvailable} from "./storage.js";
 
 
 const projectTitles = document.getElementById('projectTitles')
 const projectTemplate = document.createElement('button')
 const addProjectBtn = document.getElementById('addProject')
 const dialogTitle = document.querySelector('#dialogTitle')
+
+const currentProject = document.getElementById('currentProject')
+
 
 
 function addProjectTitle(event) {
